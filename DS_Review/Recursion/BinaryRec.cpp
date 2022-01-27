fd638463cfedca8db86ceda03aad4ff5e51f6111
James Ortiz <jamesaortiz75@gmail.com>
2022-01-26 23:35:01 -0500

    Binary Recursion Example

--- DS_Review/Recursion/BinaryRec.cpp
+++ DS_Review/Recursion/BinaryRec.cpp
@@ -1,7 +1,7 @@
 //Author: James Ortiz
 //File: BinaryRec.cpp
-//Description: An example of binary Recursion by calculating
-//the binary sum 
+//Description: An example of binary recursion by calculating
+//the binary sum in C++
 //Compile: g++ -std=c++11 -Wall BinaryRec.cpp -o BinRec
 //
 
@@ -9,6 +9,10 @@
 #include <iostream>
 using namespace std;
 
+//
+//Input: An array A and integers i and n
+//Output: The sum of the n integers in A 
+//starting at index i
 int BinarySum(std::vector<int> &A, int i, int n){
     if (n == 1){
         return A[1];
@@ -19,6 +23,10 @@ int BinarySum(std::vector<int> &A, int i, int n){
 
 int main(void){
 
-    
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    int result  = BinarySum(v, 0, 5);
+
+    std::cout << "The sum of the elements in the vector are: " << result << std::endl;
+
     return 0;
 }
